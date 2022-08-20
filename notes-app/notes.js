54f6187cd22ad38ae171e1390ef0528fae8d4633
SyedMuhammadFaheem <syed.faheem.official@gmail.com>
2022-08-20 22:05:14 +0500

    Changes

--- notes-app/notes.js
+++ notes-app/notes.js
@@ -1,6 +1,5 @@
 const fs=require('fs');
 const chalk=require('chalk');
-const getNotes=() => 'Your Notes...';
 
 
 const addNote=(title,body)=>
@@ -88,7 +87,6 @@ const saveNotes=(notes)=>{
 }
 
 module.exports={
-    getNotes:getNotes,
     addNote:addNote,
     removeNote:removeNote,
     listNotes:listNotes,
