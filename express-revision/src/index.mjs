2c234e8816d792fc083555ad78e405ac5c2b2026
Faheem <98228246+SyedMuhammadFaheem@users.noreply.github.com>
2024-01-12 19:43:45 +0500

    route-parameters

--- express-revision/src/index.mjs
+++ express-revision/src/index.mjs
@@ -3,24 +3,38 @@ import express from "express";
 const app= express()
 const PORT=process.env.PORT || 3000
 
+const users = [
+    {id:1,username:"faheem",displayName:"Faheem"},
+    {id:2,username:"umair",displayName:"Umair"},
+    {id:3,username:"rabia",displayName:"Rabia"}
+]
+
+const products = [
+    {id:1,productName:"Chicken Breasts", price:14.99}
+]
+
 app.get("/",(request,response,next)=>{
     response.status(200).send({msg:"Hello GET API!"})
     next()
 })
 
 app.get("/api/users",(request,response,next)=>{
-    response.send([
-        {id:1,username:"faheem",displayName:"Faheem"},
-        {id:2,username:"umair",displayName:"Umair"},
-        {id:3,username:"rabia",displayName:"Rabia"}
-    ])
+    response.send(users)
     next()
 })
 
 app.get("/api/products",(request,response,next)=>{
-    response.send([
-        {id:1,productName:"Chicken Breasts", price:14.99}
-    ])
+    response.send(products)
+})
+
+app.get("/api/users/:id",(request,response,next)=>{
+    const id= parseInt(request.params.id)
+    if(isNaN(id)) response.status(400).send({msg:"Bad request. Enter a numeric ID"})
+
+    const findUser= users.find((usr)=>usr.id===id)
+    if(!findUser) response.sendStatus(400)
+
+    response.send(findUser)
 })
 
 app.listen(PORT, ()=>{
